d25f05f1756de8103ee163c158c75aeee9f2ee68
ahmadhomam <156845978+ahmadhomam@users.noreply.github.com>
2025-02-07 15:52:13 +0530

    sort 0s,1s & 2s without data replacement .

--- Linkedlist/LL_questions.cpp
+++ Linkedlist/LL_questions.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 using namespace std ;
-int main(){
-    cout<<"hellow world how are you"<<endl ;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node* sortList(Node *head){
+   Node* zerohead = new Node(0) ;
+   Node* zerotail = zerohead ;
+   Node* onehead = new Node(0) ;
+   Node* onetail = onehead ;
+   Node* twohead = new Node(0) ; 
+   Node* twotail = twohead ;
+
+   Node* temp = head ;
+   while(temp != NULL){
+       if(temp->data == 0){
+           zerotail->next = temp ;
+           zerotail = zerotail->next ;
+       }
+       else if(temp->data == 1 ){
+           onetail->next = temp ;
+           onetail = onetail->next ;
+       }
+       else{
+           twotail->next = temp ;
+           twotail = twotail->next ;
+       }
+       temp = temp->next ;
+   }
+   if(onehead->next != NULL){
+       zerotail->next = onehead->next ;
+   }
+   else{
+       zerotail->next = twohead->next ;
+   }
+   onetail->next = twohead->next ;
+   twotail->next = NULL ;
+
+   head = zerohead->next  ;
+   return head ;
 }
