1cf06f83f58cc7373547921f35866a4088ed5462
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2025-02-07 15:58:17 +0530

    sort 0s,1s with data replacement

---
+++ Linkedlist/LL_questions.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std ;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+//Approach 1 with data replacement : T.C =O(n) + O(n) = O(n) and S.C = O(1) ;
+Node* sortList(Node *head){
+    int zerocount = 0 ;
+    int onecount = 0 ;
+    int twocount = 0 ;
+    Node *temp = head ;
+    while(temp != NULL){
+        if(temp->data == 0){
+            zerocount++ ;
+        }
+        else if(temp->data == 1){
+            onecount++ ;
+        }
+        else{
+            twocount++ ;
+        }
+        temp = temp->next ;
+    }
+    temp = head ;
+    while(temp != NULL){
+        for (int i =0 ;i<zerocount;i++){
+            temp->data = 0 ;
+            temp= temp->next ;
+        }
+        for(int i =0;i<onecount;i++){
+            temp->data = 1 ;
+            temp= temp->next ;
+
+        }
+        for(int i=0;i<twocount;i++){
+            temp->data =2 ;
+            temp= temp->next ;
+
+        }
+    }
+    return head ;
+}
+
+//Approach 2 without data replacement : T.C = O(n) and S.C = O(1) ;
+Node* sortList(Node *head){
+   Node* zerohead = new Node(0) ;
+   Node* zerotail = zerohead ;
+   Node* onehead = new Node(0) ;
+   Node* onetail = onehead ;
+   Node* twohead = new Node(0) ; 
+   Node* twotail = twohead ;
+
+   Node* temp = head ;
+   while(temp != NULL){
+       if(temp->data == 0){
+           zerotail->next = temp ;
+           zerotail = zerotail->next ;
+       }
+       else if(temp->data == 1 ){
+           onetail->next = temp ;
+           onetail = onetail->next ;
+       }
+       else{
+           twotail->next = temp ;
+           twotail = twotail->next ;
+       }
+       temp = temp->next ;
+   }
+   if(onehead->next != NULL){
+       zerotail->next = onehead->next ;
+   }
+   else{
+       zerotail->next = twohead->next ;
+   }
+   onetail->next = twohead->next ;
+   twotail->next = NULL ;
+
+   head = zerohead->next  ;
+   return head ;
+}
