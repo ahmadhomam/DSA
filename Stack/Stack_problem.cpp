efefbc7cbd4ed24ef44ae389787f4e8e5696ab3a
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2025-03-17 03:22:15 +0530

    deleting the middle element using recursion.

--- Stack/Stack_problem.cpp
+++ Stack/Stack_problem.cpp
@@ -20,3 +20,22 @@ int main(){
     cout<<"the reverse string is : "<<ans<<endl; 
 
 }
+
+// DELETING THE MIDDLE ELEMENT
+void solve (stack<int>&inputStack, int N,int count){
+    if(count == N/2){
+       inputStack.pop() ;
+       return ;
+    }
+ 
+    int temp = inputStack.top() ;
+    inputStack.pop() ;
+    solve(inputStack,N,count+1) ;
+    inputStack.push(temp) ;
+ }
+ void deleteMiddle(stack<int>&inputStack, int N){
+     
+    // Write your code here
+    int count =0 ;
+    solve(inputStack,N,count) ;
+ }
