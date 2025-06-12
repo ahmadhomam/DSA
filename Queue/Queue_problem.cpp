2873a91bcf5f490f70bb7361f23c4fe53dfc8a1b
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2025-06-13 05:22:32 +0530

    First -ve integer in window of size k .

--- Queue/Queue_problem.cpp
+++ Queue/Queue_problem.cpp
@@ -3,54 +3,36 @@ using namespace std ;
 #include <queue>
 #include <stack>
 
-// #APPROACH 1 ( STACK AS A AUXILARY SPACE )
-class Solution {
-  public:
-    queue<int> rearrangeQueue(queue<int> q) {
-        // code here
-        stack<int > s ;
-        int second = q.size()/2 ;
-        for(int i=0;i<second ;i++){
-            s.push(q.front()) ;
-            q.pop() ; 
-        }
-        while(!s.empty()){
-            q.push(s.top()) ;
-            s.pop() ;
-        }
-        for(int i=0;i<second;i++){
-            int temp = q.front() ;
-            q.push(temp);
-            q.pop() ;
-        }
-        for(int i=0;i<second;i++){
-            s.push(q.front()) ;
-            q.pop() ;
-        }
-        while(!s.empty()){
-            int temp = q.front() ;
-            q.push(s.top()) ;
-            s.pop() ;
-            q.push(temp) ;
-            q.pop() ;
-        }
-        return q ;
-    }
-};
+//FIRST -VE INTEGER IN WINDOW OF SIZE K
+vector<int> firstNegative(vector<int> arr, int n, int k) {
+	queue<int> q ;
+	vector<int>ans ;
 
-//#APPROACH 2 ( QUEUE AS AUXILARY SPACE )
-void interLeaveQueue(queue < int > & q) {
-    int second = q.size()/2 ;
-    queue <int> newq ;
-    for(int i=0 ;i<second;i++){
-        newq.push(q.front()) ;
-        q.pop() ;
-    }
-    while(!newq.empty()){
-        int temp = q.front() ;
-        q.push(newq.front()) ;
-        newq.pop() ;
-        q.push(temp) ;
-        q.pop() ;
-    }
+	for(int i=0;i<k;i++){
+		if(arr[i]<0){
+			q.push(i) ;
+		}
+	}
+	if(q.empty())
+	ans.push_back(0) ;
+	else
+	ans.push_back(arr[q.front()]) ;
+
+	for(int i=k;i<arr.size();i++){
+			//removal ;
+			if(!q.empty() && i-q.front() >= k){
+				q.pop() ;
+			}
+			//addition
+			if( arr[i]<0){
+				q.push(i) ;
+			}
+			if(q.empty())
+			ans.push_back(0) ;
+			
+			else
+			ans.push_back(arr[q.front()]) ;
+	}
+	return ans ;
+
 }
