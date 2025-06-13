b554bf797a597eb0f37d1c868f31bd93c7fd0003
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2025-06-13 05:31:03 +0530

    First Non-Repeating character in stream

--- Queue/Queue_problem.cpp
+++ Queue/Queue_problem.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
 using namespace std ;
+#include <unordered_map>
 #include <queue>
+#include <string>
 #include <stack>
 
 //FIRST -VE INTEGER IN WINDOW OF SIZE K
@@ -36,3 +37,33 @@ vector<int> firstNegative(vector<int> arr, int n, int k) {
 	return ans ;
 
 }
+
+//FIRST NON REPEATING CHARACTER IN STREAM OF STRING 
+class Solution {
+  public:
+    string FirstNonRepeating(string &s) {
+         unordered_map<char,int> count ;
+         queue<char>q ;
+         string ans = "" ;
+         
+         for(int i= 0;i<s.length();i++){
+             char ch = s[i] ;
+             count[ch]++ ;
+             
+             q.push(ch) ;
+             while(!q.empty()){
+                 if(count[q.front()] > 1){
+                     q.pop() ;
+                 }
+                 else{
+                     ans.push_back(q.front()) ;
+                     break ;
+                 }
+             }
+             if(q.empty())
+             ans.push_back('#') ;
+             
+         }
+         return ans ;
+    }
+};
