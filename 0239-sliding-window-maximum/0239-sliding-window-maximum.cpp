4901db9af9c0981d102bd76a1b9953cb101526f7
Homam Ahmad Kazimi <156845978+ahmadhomam@users.noreply.github.com>
2026-07-19 17:42:56 +0530

    Time: 16 ms (85.21%), Space: 139.2 MB (44.96%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size() ;
+        deque<int> deq ;
+        vector<int> ans ;
+
+        for(int i=0;i<n;i++){
+
+            if(!deq.empty() && deq.front() == i-k)
+            deq.pop_front() ;
+
+            while(!deq.empty() && nums[deq.back()] < nums[i]){
+                deq.pop_back() ;
+            }
+
+            deq.push_back(i) ;
+
+            if(i >= k-1)
+            ans.push_back(nums[deq.front()]) ;
+        }
+
+        return ans ;
+    }
+};
