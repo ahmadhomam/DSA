4e05adb012df972541a4bbe28563e08f86c7a37f
Homam Ahmad Kazimi <156845978+ahmadhomam@users.noreply.github.com>
2026-08-09 00:03:10 +0530

    Time: 0 ms (100%), Space: 97.4 MB (61.4%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,13 +3,14 @@ public:
     int maxProfit(vector<int>& prices) {
         int maxprofit = 0 ;
         int profit = 0  ;
-        int minprice = prices[0] ;
+        int minprice = INT_MAX ;
 
         for(int i=0;i<prices.size();i++){
+            minprice = min(minprice,prices[i]) ;
+            
             profit  = prices[i] - minprice  ;
             maxprofit = max(maxprofit,profit) ;
 
-            minprice = min(minprice,prices[i]) ;
         }
         return maxprofit  ;
     }
