a30f4dbf0cc58be108bb6056c4f8a6f945b84699
Homam Ahmad Kazimi <156845978+ahmadhomam@users.noreply.github.com>
2026-08-29 23:38:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Sum Pairs in Sorted/count-sum-pairs-in-sorted.cpp
+++ Difficulty: Easy/Count Sum Pairs in Sorted/count-sum-pairs-in-sorted.cpp
@@ -1,28 +1,33 @@
 class Solution {
   public:
     int countPairs(vector<int> &arr, int target) {
-        // Complete the function
-        int i=0; int j= arr.size()-1;
-        int cnt = 0;
+        // Code Here
+        int i=0 ;
+        int j = arr.size()-1 ;
+        int cnt =0 ;
+        
         while(i<=j){
-            int sum = arr[i]+arr[j];
-            if(sum == target){
+            int sum = arr[i] + arr[j] ;
+            if(sum == target) {
                 if(arr[i] == arr[j]){
-                    int n = j-i+1;
-                    cnt += (n*(n-1))/2;
-                    break;
-                }else {
-                    int prev_i =i, prev_j = j;
-                    while(i<j && arr[i]==arr[prev_i]) i++;
-                    while(j>=i && arr[j]== arr[prev_j]) j--;
-                    cnt += (i-prev_i)*(prev_j-j);
+                    int n = j-i+1 ;
+                    cnt += (n*(n-1))/2 ;
+                    break ;
+                    
+                }
+                else{
+                    int prev_i = i ;
+                    int prev_j = j ;
+                    while(i<j && arr[i] == arr[prev_i]) i++ ;
+                    while(i<=j && arr[j] == arr[prev_j]) j-- ;
+                    cnt += (i-prev_i)*(prev_j-j) ;
                 }
             }
-            if(sum>target){
-                j--;
-            }
-            if(sum<target) i++;
+            else if(sum > target)
+            j-- ;
+            else
+            i++ ;
         }
-        return cnt;
+        return cnt ;
     }
 };
