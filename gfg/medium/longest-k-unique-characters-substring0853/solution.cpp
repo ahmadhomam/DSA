6d8370133e30bf7d8c5bc9f41d730c04f364edfb
Homam Ahmad Kazimi <156845978+ahmadhomam@users.noreply.github.com>
2026-09-20 17:08:05 +0530

    ✅ Longest Substring with K Uniques | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-20

--- gfg/medium/longest-k-unique-characters-substring0853/solution.cpp
+++ gfg/medium/longest-k-unique-characters-substring0853/solution.cpp
@@ -1,37 +1,37 @@
 class Solution {
-  public:
-    int longestKSubstr(string &s, int k) {
-        // code here
-        int n = s.size() ;
-        unordered_map<char,int> freq ;
-        int maxsize = -1 ;
-        
-        int i =0 , j=0 ;
-        
-        while(j< n){
-            //calculations 
-            freq[s[j]]++ ;
-            
-            //if invalid wala case : longest size nikalna hai
-            while (freq.size() > k) {
-                            freq[s[i]]--;
-
-                            if (freq[s[i]] == 0) {
-                                freq.erase(s[i]);
-                            }
-
-                            i++;
-                        }
-            
-            //found valid 
-            if(freq.size() == k){
-                maxsize = max(maxsize, j-i+1) ;
-            }
-            
-            j++ ;
-
-        }
-        return maxsize ;
-        
-    }
+	public:
+	int longestKSubstr(string &s, int k) {
+		// code here
+		int n = s.size() ;
+		unordered_map<char, int> freq ;
+		int maxsize = -1 ;
+		
+		int i = 0, j = 0 ;
+		
+		while (j<n) {
+			// calculations
+			freq[s[j]]++ ;
+			
+			// if invalid wala case : longest size nikalna hai
+			while (freq.size() > k) {
+				freq[s[i]]--;
+				
+				if (freq[s[i]] == 0) {
+					freq.erase(s[i]);
+				}
+				
+				i++;
+			}
+			
+			// found valid
+			if (freq.size() == k) {
+				maxsize = max(maxsize, j - i + 1) ;
+			}
+			
+			j++ ;
+			
+		}
+		return maxsize ;
+		
+	}
 };
